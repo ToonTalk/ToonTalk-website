3cfdc89118085e7e055fe6131cfe7a5bb3fbb484
Ken Kahn <toontalk@gmail.com>
2024-04-27 11:56:48 +0200

    Initial commit

---
+++ Swedish/_vti_cnf/BNCD.js
@@ -0,0 +1,6 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|21 Feb 2008 09:07:00 -0000
+vti_extenderversion:SR|6.0.2.6551
+vti_cacheddtm:TX|21 Feb 2008 09:07:00 -0000
+vti_filesize:IR|913
+vti_backlinkinfo:VX|Swedish/whatlang.htm Swedish/whatvce.htm Swedish/jumpopt.htm Swedish/winsize.htm Swedish/resoluti.htm Swedish/soundfx.htm Swedish/marttalk.htm Swedish/mousebut.htm Swedish/option1.htm Swedish/usertype.htm
---
+++ Swedish/strings.js
@@ -0,0 +1,15 @@
+// JScript source code
+
+// common strings that need to be translated for MS Agent to work with ToonTalk demos
+
+var languageCode = 0x041D;
+// other settings are SE: 0x041D; PT: 0x0816; IT: 0x0410; US: 0x0409;  0x0809; // UK English
+var useAgents = true; // so can be easily turned off
+
+var greeting = "Hej!|Tjena!|Hallå där!";
+var bye = "Hej då!|Adjö!|Ses!";
+var clickedFirstReactions = "Låt bli, jag är kittlig!|Ta det lugnt med pekaren!";
+var clickedFinalReactions = "Gör du om det där, så drar jag!";
+
+//agentStart(); 
+// Remove // above when appropriate that pages are read even if the reading icon isn't clicked. E.g. for the visually impaired.
